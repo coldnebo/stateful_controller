39a1ebc0971b6d7e79d52a38a9d7a2eddc96279e
Larry Kyrala <larry.kyrala@gmail.com>
2015-12-01 11:36:57 -0500

    initial working state. (been testing within the rails app, may have to return to the unit tests in a bit)

--- lib/stateful_controller.rb
+++ lib/stateful_controller.rb
@@ -1,5 +1,95 @@
-require "stateful_controller/version"
+require 'stateful_controller/version'
+require 'active_support/concern'
+require 'aasm'
+require 'ostruct'
 
 module StatefulController
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class State
+    attr_accessor :current_state
+  end
+
+  included do
+    include AASM
+    attr_reader :state
+    before_filter :__setup_and_process
+    after_filter :__finish
+  end
+
+  # StatefulController always defines a special action called 'next' that goes to the next available transition.
+  def next
+    # this may rely on events having exactly one transition... otherwise, we may need an argument? (TBD) 
+    next_events = aasm.events(permitted: true).map(&:name)
+    __debug("choosing first from next events: #{next_events.inspect}")
+    next_event = next_events.first
+    __process(next_event)
+    self.send(next_event)
+  end
+
+  protected
+
+  def initialize
+    super
+  end
+
+  def default_render(*args)
+    render aasm.current_state
+  end
+
+  # before filter for setup
+  def __setup_and_process
+    @state = load_state
+    __debug("load_state returned #{@state.inspect}")
+    raise ArgumentError, "load_state() must return a StatefulController::State or subclass." unless @state.kind_of?(State)
+
+    # allow the StatefulController to retain it's current state across requests (since controller instances are created per request in Rails)
+    if state.current_state.nil?
+      state.current_state = aasm.current_state  # if load_state didn't recover a current_state, then use the aasm initial.
+    else
+      aasm.current_state = state.current_state  # otherwise, set the aasm.current_state to the loaded current_state.
+    end
+
+    # rails action should always be something convertible to a symbol
+    event = params[:action].to_sym
+
+    # next is a special action, don't try to send the event.
+    return if event == :next
+
+    __process(event)
+  end
+
+  def __process(event)
+    # manually trigger the event (while we consider the aasm events as "actions", we reserve the methods in the 
+    # controller as actual actions instead of aasm event triggering sugar.)
+    # adapted from ./lib/aasm/base.rb:85
+
+    # while the aasm gem supports multiple state machines per class, StatefulController always uses only one state machine, the default.
+    # so we may substitute :default for @name in the DSL impl:
+    # original line:  aasm(:#{@name}).current_event = :#{name}
+    aasm.current_event = event
+
+    # also, aasm supports arguments for events, but because StatefulController models Rails controller actions, no arguments or blocks are supported.
+    # original line:  aasm_fire_event(:#{@name}, :#{name}, {:persist => false}, *args, &block)
+    aasm_fire_event(:default, event, {persist: false}, [])
+  end
+
+  def __finish
+    state.current_state = aasm.current_state
+    __debug("calling save_state with #{@state.inspect}")
+    save_state
+  end
+
+  def load_state
+    raise NotImplementedError, "Controllers that include StatefulController should define the load_state() method."
+  end
+
+  def save_state
+    raise NotImplementedError, "Controllers that include StatefulController should define the save_state() method."
+  end    
+
+  def __debug(msg)
+    Rails.logger.debug("DEBUG [StatefulController]: #{msg}")
+  end
+
 end
--- test/stateful_controller_test.rb
+++ test/stateful_controller_test.rb
@@ -1,11 +1,50 @@
 require 'test_helper'
 
 class StatefulControllerTest < Minitest::Test
+  
+  class MyController
+    include StatefulController
+
+    # states are 'views' and transitions are 'actions'
+    aasm do 
+      state :sleeping, initial: true
+      state :running
+      state :cleaning
+    
+      event :run do
+        transitions from: :sleeping, to: :running
+      end
+
+      event :clean do 
+        transitions from: :running, to: :cleaning
+      end
+
+      event :sleep do 
+        transitions from: [:running, :cleaning], to: :sleeping
+      end
+    end
+
+    # actions 
+    def run
+    end
+
+    def clean
+    end
+
+    def sleep
+    end
+  end
+
+
   def test_that_it_has_a_version_number
     refute_nil ::StatefulController::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+
+  def test_it_has_its_own_state
+    instance = MyController.new
+    refute instance.state.nil?
+    assert instance.state.current_state == :sleeping
   end
+  
 end
